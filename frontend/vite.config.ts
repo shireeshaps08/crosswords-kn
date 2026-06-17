60d436db37e5c0eb678fb937ed834eaafd6d17b4
Shireesha PS <shireesha.ps@printersmysore.co.in>
2026-06-17 14:08:40 +0530

    deploy this site

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   resolve: { alias: { '@': '/src' } },
+  base: '/crosswords-kn/',
   server: {
     proxy: {
       '/api': { target: 'http://localhost:4000', changeOrigin: true }
